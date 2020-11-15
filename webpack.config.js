7922bad1ce274e2813529d9aefd9a6f1f34ff400
Berkan Unal <Berkanunal@gmail.com>
2020-11-15 09:09:20 +0300

    Add basic webpack tsconfig babel configs

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+const WebpackWatchedGlobEntries = require("webpack-watched-glob-entries-plugin");
+
+module.exports = {
+  entry: WebpackWatchedGlobEntries.getEntries([
+    path.resolve(__dirname, "src/**/*.ts"),
+  ]),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    iife: false,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new WebpackWatchedGlobEntries()],
+  optimization: {
+    minimize: false,
+  },
+};
